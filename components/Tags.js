8f222f06d30647913c8e86f0512799dee4ab5eb3
Jesus Ardila <jesusaurelioardilasanchez7@gmail.com>
2023-03-27 12:36:05 -0500

    Creating browser components and helpers

---
+++ components/Tags.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tags = () => {
+  return (
+    <div>
+        <h2>Title Tags</h2>
+        <section>
+            <p>este es un parrafo</p>
+        </section>
+    </div>
+  )
+}
+
+export default Tags
